45b4c7f4f926cd34affff99b1c77a848f79553ab
xylonity <42015327+Xylonity@users.noreply.github.com>
2025-12-21 18:03:50 +0100

    🐛 Fixed forge not detecting emi screen stacks

--- common/src/main/java/dev/xylonity/tooltipoverhaul/client/style/inner/GradientInnerOverlay.java
+++ common/src/main/java/dev/xylonity/tooltipoverhaul/client/style/inner/GradientInnerOverlay.java
@@ -2,7 +2,7 @@ package dev.xylonity.tooltipoverhaul.client.style.inner;
 
 import dev.xylonity.tooltipoverhaul.client.layer.impl.InnerOverlayLayer;
 import dev.xylonity.tooltipoverhaul.client.render.TooltipContext;
-import net.minecraft.client.gui.GuiGraphics;
+import dev.xylonity.tooltipoverhaul.client.util.RenderUtils;
 import net.minecraft.world.phys.Vec2;
 
 import java.awt.*;
@@ -26,23 +26,11 @@ public class GradientInnerOverlay implements InnerOverlayLayer {
         int width = (int) (context.getTooltipSize().x + 5);
         int height = (int) (context.getTooltipSize().y + 4);
 
-        renderFrameGradient(context.getGraphics(), x0, y0 + 1, width, height - 2, color1, color2, color3);
+        RenderUtils.renderFrameGradient(context.getGraphics(), x0, y0 + 1, width, height - 2, color1, color2, color3);
 
         // Top and bottom lines
         context.getGraphics().fill(x0, y0, x0 + width, y0 + 1, color1);
         context.getGraphics().fill(x0, y0 + height - 1, x0 + width, y0 + height, color3);
-    }
-
-    private static void renderFrameGradient(GuiGraphics graphics, int x, int y, int width, int height, int c1, int c2, int c3) {
-        int mid = height / 2;
-
-        // Left border (top and bottom sections)
-        graphics.fillGradient(x, y, x + 1, y + mid, c1, c2);
-        graphics.fillGradient(x, y + mid, x + 1, y + height, c2, c3);
-
-        // Right border (top and bottom sections)
-        graphics.fillGradient(x + width - 1, y, x + width, y + mid, c1, c2);
-        graphics.fillGradient(x + width - 1, y + mid, x + width, y + height, c2, c3);
     }
 
 }
